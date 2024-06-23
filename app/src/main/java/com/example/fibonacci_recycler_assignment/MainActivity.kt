e29924bba4fb05d0221b9ea459dd8f63563757d3
Kur-Achan <achananyiethkur@gmail.com>
2024-06-23 19:50:54 +0300

    add changes

--- app/src/main/java/com/example/fibonacci_recycler_assignment/MainActivity.kt
+++ app/src/main/java/com/example/fibonacci_recycler_assignment/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.fibonacci_recycler_assignment
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.fibonacci_recycler_assignment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var binding: ActivityMainBinding
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val numbers = fibonacci(100)
+        binding.rvNumbers.layoutManager = LinearLayoutManager(this)
+
+        val numberAdapter = NumbersRecyclerViewAdapter(numbers)
+        binding.rvNumbers.adapter = numberAdapter
+
+
+
+
+
+
+    }
+
+    fun fibonacci(n: Int): List<Int> {
+        val numbers = mutableListOf(0, 1)
+        while (numbers.size < n) {
+            numbers.add(numbers[numbers.lastIndex] + numbers[numbers.lastIndex - 1])
         }
+        return numbers
     }
 }
--- app/src/main/java/com/example/fibonacci_recycler_assignment/fibonacci_assignment.kt
+++ app/src/main/java/com/example/fibonacci_recycler_assignment/fibonacci_assignment.kt
@@ -1,20 +1,34 @@
 package com.example.fibonacci_recycler_assignment
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
-class fibonacci_assignment : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_fibonacci_assignment)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+class NumbersRecyclerViewAdapter (var numbers:List<Int>):RecyclerView.Adapter<NumbersViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumbersViewHolder {
+        val numberView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.activity_fibonacci_assignment,parent,false)
+        return NumbersViewHolder(numberView)
     }
+
+    override fun onBindViewHolder(holder: NumbersViewHolder, position: Int) {
+        holder.tvNumber.text = numbers[position].toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return numbers.size
+    }
+
+}
+
+class NumbersViewHolder(numberView: View): RecyclerView.ViewHolder(numberView){
+    val tvNumber = numberView.findViewById<TextView>(R.id.tvNumbers)
 }
